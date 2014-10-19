018f97ef1fae3fe87a4398d3c1182490ded9038a
Gilbert Fu <gilbertfu@gmail.com>
2014-10-19 00:15:52 +0000

    Initial Base Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BrunchworkApp::Application.config.secret_key_base = '22472d7389ff80d2e5064bf98e7a3ae04ee0b3f4110c682adb118e423ef18b50e76f371f8cc0d9b8bc2f14f59474c9d696e3697b01d4d6e625c1438d7e0c7c7f'
