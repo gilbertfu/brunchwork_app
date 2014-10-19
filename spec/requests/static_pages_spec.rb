03af221ca1edde1da7a0158ce428a4b39dab678a
Gilbert Fu <gilbertfu@gmail.com>
2014-10-19 20:51:57 +0000

    fix static page test case

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,35 +17,35 @@ describe "StaticPages" do
   
   describe "Help page" do
     it "should have content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     it "should have title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title('BrunchWork | Help')
     end
   end
   
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
     
     it "should have title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title('BrunchWork | About Us')
     end
   end
   
   describe "Contact page" do
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
     
     it "should have title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title('BrunchWork | Contact')
     end
   end
